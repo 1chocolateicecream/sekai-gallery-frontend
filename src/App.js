a1b15a5a7278331aef3c6d4c8ddc42ba2c6edbb2
alisa <alisa.zavjalkina@icloud.com>
2025-11-07 23:45:35 +0200

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import React, { useState, useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
